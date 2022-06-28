c2464dcac07789214e7cbe50d6bbeb40934406b1
takumi <konnitiha@outlook.com>
2022-06-28 15:49:34 +0900

    fix

--- Faraday/two_dim/module/lib/regularizer.py
+++ Faraday/two_dim/module/lib/regularizer.py
@@ -15,13 +15,13 @@ class ShiftL1Regularizer(tf.keras.regularizers.Regularizer):
 
 
 @tf.keras.utils.register_keras_serializable(package='Custom', name='shift_l2')
-class ShiftL1Regularizer(tf.keras.regularizers.Regularizer):
+class ShiftL2Regularizer(tf.keras.regularizers.Regularizer):
     def __init__(self, l2=0., shift=0.):
         self.l1 = l2
         self.shift = shift
 
     def __call__(self, x):
-        return self.l1 * tf.math.reduce_sum((self.shift**2 - x**2))
+        return self.l1 * tf.math.reduce_sum(np.abs((self.shift**2 - x**2)))
 
     def get_config(self):
         return {'l2': float(self.l1), 'shift': float(self.shift)}
